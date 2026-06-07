de4415db33d59a8e4a01b12bcb2f4ce8a9216774
Hemanth <hemanth.smart7784@gmail.com>
2026-06-07 15:08:07 +0530

    6thh

--- src/main/java/ForGit/ForGit/HellpGit.java
+++ src/main/java/ForGit/ForGit/HellpGit.java
@@ -10,7 +10,8 @@ public class HellpGit {
 		System.out.println("Prabhas");
 		System.out.println("Hemanth");
 		int a=20,b=30;
-		System.out.println("additon ="+a+b);
+		System.out.println("additon ="+(a+b));
+		System.out.println("sub ="+(b-a));
 
 	}
 
