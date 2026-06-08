7e1a55b6b4f572b69893db54a58ec22f131e6758
Hemanth <hemanth.smart7784@gmail.com>
2026-06-08 23:36:25 +0530

    in

--- src/main/java/ForGit/ForGit/HellpGit.java
+++ src/main/java/ForGit/ForGit/HellpGit.java
@@ -3,10 +3,7 @@ package ForGit.ForGit;
 public class HellpGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello git1");
-		System.out.println("Hello git2");
-		System.out.println("Hello git3");
-		System.out.println("Hello git4");
+		
 		System.out.println("Prabhas");
 		System.out.println("Hemanth");
 		int a=20,b=30;
